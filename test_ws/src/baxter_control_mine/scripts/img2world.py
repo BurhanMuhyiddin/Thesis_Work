b18f7abd7b6dccb0a2f0039ec7d9db1ffd796838
BurhanMuhyiddin <burhanbhos1997@gmail.com>
2022-02-14 13:33:06 +0100

    check with undistroted model

--- test_ws/src/baxter_control_mine/scripts/img2world.py
+++ test_ws/src/baxter_control_mine/scripts/img2world.py
@@ -9,8 +9,7 @@ from sensor_msgs.msg import Image, CameraInfo
 import tf2_ros
 import tf2_geometry_msgs
 
-from geometry_msgs.msg import Pose, Point, Quaternion, PoseStamped, PointStamped
-from std_msgs.msg import Header
+from geometry_msgs.msg import Pose, PointStamped
 
 from baxter_msgs_mine.msg import GoToPointAction
 from baxter_msgs_mine.msg import GoToPointGoal
@@ -32,12 +31,12 @@ class IMG2WORLD:
 
         self.cam_info = None
 
-        self.desiredPix = (581, 323)
+        self.desiredPix = (277, 381)
 
-        self.sf = "left_hand_camera_axis"
+        self.sf = "left_hand_camera"
         self.tf = "base"
 
-        self.tf_buffer = tf2_ros.Buffer(rospy.Duration(100.0))  # tf buffer length
+        self.tf_buffer = tf2_ros.Buffer(rospy.Duration(100.0))
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
 
         self.table_dist = -0.1538229
@@ -64,66 +63,40 @@ class IMG2WORLD:
 
                 depth = abs(transform.transform.translation.z - self.table_dist)
 
-                # gl = Pose(Point(x, y, z), Quaternion(0.0983495, 0.989932, 0.030586, 0.0970911))
-
-                # ps = PointStamped()
-                # ps.header.stamp = rospy.Time(0)
-                # ps.header.frame_id = 'left_hand_camera_axis'
-                # ps.point.x = x
-                # ps.point.y = y
-                # ps.point.z = z
-
-
-
-                # hd = Header()
-                # hd.frame_id = "left_hand_camera_axis"
-                # goal_stamped = PoseStamped()
-                # goal_stamped.header = hd
-                # goal_stamped.pose = gl
-
-
-                # point_transformed = tf2_geometry_msgs.do_transform_point(ps, transform)
-                # pp = Pose()
-                # pp.position.x = ps.point.x
-                # pp.position.y = ps.point.y 
-                # pp.position.z = z
-                # pp.orientation.x = 0.0411739
-                # pp.orientation.y = 0.994043
-                # pp.orientation.z = 0.0261063
-                # pp.orientation.w = 0.0974731
-                # print(pp)
-
                 px = self.desiredPix[0]
                 py = self.desiredPix[1]
 
-                cx = self.cam_model.cx()
-                cy = self.cam_model.cy()
-                fx = self.cam_model.fx()
-                fy = self.cam_model.fy()
+                #try to include distortion also
+
+                K = self.cam_model.intrinsicMatrix()
+                
+                cx = K[0,2]
+                cy = K[1,2]
+                fx = K[0,0]
+                fy = K[1,1]
 
                 x = depth * (px - cx) / fx
                 y = depth * (py - cy) / fy
                 z = depth
 
-                print((x, y, z))
-
                 ps = PointStamped()
                 ps.header.stamp = rospy.Time(0)
-                ps.header.frame_id = 'left_hand_camera_axis'
+                ps.header.frame_id = "left_hand_camera"
                 ps.point.x = x
                 ps.point.y = y
-                ps.point.z = 0.0612
+                ps.point.z = z
 
                 point_transformed = tf2_geometry_msgs.do_transform_point(ps, transform)
                 pp = Pose()
-                pp.position.x = ps.point.x
-                pp.position.y = ps.point.y 
+                pp.position.x = point_transformed.point.x
+                pp.position.y = point_transformed.point.y 
                 pp.position.z = 0.0612
-                pp.orientation.x = 0.0411739
-                pp.orientation.y = 0.994043
-                pp.orientation.z = 0.0261063
-                pp.orientation.w = 0.0974731
+                pp.orientation.x = 0.017546
+                pp.orientation.y = 0.994616
+                pp.orientation.z = 0.0223233
+                pp.orientation.w = 0.0996664
                 # print(pp)
+
                 if not self.flag:
                     self.gtp_cl.send_goal(GoToPointGoal(pp))
 
@@ -133,9 +106,6 @@ class IMG2WORLD:
             except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
                 self.rate.sleep()
                 pass
-
-        # cv2.imshow("mouseRGB", cv_image)
-        # cv2.waitKey(3)
 
 def main():
     rospy.init_node('image_converter')
