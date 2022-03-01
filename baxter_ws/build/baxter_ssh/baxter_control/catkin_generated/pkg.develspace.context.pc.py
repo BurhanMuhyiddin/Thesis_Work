1a28b94a25f8e41d164d6bb61c8905a1f10be2c4
BurhanMuhyiddin <burhanbhos1997@gmail.com>
2022-03-01 15:43:51 +0100

    baxter_ws deleted

--- baxter_ws/build/baxter_ssh/baxter_control/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/lar/ros/baxter_ws/src/baxter_ssh/baxter_control/include".split(';') if "/home/lar/ros/baxter_ws/src/baxter_ssh/baxter_control/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "std_msgs;roscpp;baxter_core_msgs;controller_manager;transmission_interface;hardware_interface;sensor_msgs;joint_limits_interface;trajectory_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lbaxter_utilities;-lbaxter_to_csv;-larm_interface".split(';') if "-lbaxter_utilities;-lbaxter_to_csv;-larm_interface" != "" else []
-PROJECT_NAME = "baxter_control"
-PROJECT_SPACE_DIR = "/home/lar/ros/baxter_ws/devel"
-PROJECT_VERSION = "0.3.0"
--- baxter_ws/devel/share/common-lisp/ros/move_rt/msg/_package_ExecutingTrajectoryActionFeedback.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package move_rt-msg)
-(cl:export '(HEADER-VAL
-          HEADER
-          STATUS-VAL
-          STATUS
-          FEEDBACK-VAL
-          FEEDBACK
-))
