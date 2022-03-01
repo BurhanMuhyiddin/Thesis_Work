19ec0ac634a090471f3b2fd155befdce79f77bb7
BurhanMuhyiddin <burhanbhos1997@gmail.com>
2022-03-01 15:10:49 +0100

    baxter_ws removed

--- baxter_ws/build/move_rt/CMakeFiles/clean_test_results_move_rt.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/clean_test_results_move_rt"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/clean_test_results_move_rt.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
